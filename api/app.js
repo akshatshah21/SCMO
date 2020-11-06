20f78d8245389396ab0b4d748f1554006366fcd9
Akshat Shah <akshatshah_21@yahoo.in>
2020-11-06 22:54:42 +0530

    Express application set up
    Doesn't do anything yet!

---
+++ api/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const PORT = process.env.port || 5000;
+
+app.get("/", (req, res) => res.send("Hello World"));
+
+app.listen(PORT, () => console.log(`App running on port ${PORT}`));
