65a7d2226d66404b9253532496fc69823e168a6d
Akshat Shah <akshatshah_21@yahoo.in>
2020-11-07 09:53:08 +0530

    React Router set up and directory structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Link, BrowserRouter } from "react-router-dom";
+
+import Warehouse from "./components/Warehouse";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/warehouse">Warehouse</Link>
+            </li>
+          </ul>
+        </nav>
+        {/* <Route path="/" component={Home}  />
+        <Route path="/login" component={Login} /> */}
+        <Route path="/warehouse" component={Warehouse} />
+      </div>
+    </BrowserRouter>
   );
 }
 
