7dfbe3221e37c83d3c2af56f12f3be14020a5ed0
Akshat Shah <akshatshah_21@yahoo.in>
2020-11-11 14:58:15 +0530

    SendShipmentrequests for "code" on form submit
    Display the code in a Modal, and Okaying it redirects to Storage Center

--- client/src/components/storageCenter/SendShipment.jsx
+++ client/src/components/storageCenter/SendShipment.jsx
@@ -2,30 +2,56 @@ import React, { useEffect, useState } from 'react'
 import axios from "axios";
 import M from "materialize-css";
 
-export default function SendShipment() {
+export default function SendShipment({ history }) {
   const [recipients, setRecipients] = useState([]);
   const [products, setProducts] = useState([]);
   const [formData, setFormData] = useState({});
+  const [code, setCode] = useState("");
+
   useEffect(() => {
-    const effect = async () => {
-    // get list of recipients, products and then set state
-    
-    // Dummy data
-    let { data: recipientList } = await axios("https://jsonplaceholder.typicode.com/users");
-    let { data: productList } = await axios("https://jsonplaceholder.typicode.com/todos");
-    setRecipients(() => recipientList.map(recipient => ({name: recipient.name, id: recipient.id}) ));
-    setProducts(() => productList.map(product => ({name: product.title, id: product.id }) ));
-    
-    var selects = document.querySelectorAll('select');
-    M.FormSelect.init(selects);
+    const selectEffect = async () => {
+      // get list of recipients, products and then set state
+      /*
+        GET /stage
+        GET /product
+      */
+
+      // Dummy data for now
+      let { data: recipientList } = await axios("https://jsonplaceholder.typicode.com/users");
+      let { data: productList } = await axios("https://jsonplaceholder.typicode.com/todos");
+      setRecipients(() => recipientList.map(recipient => ({name: recipient.name, id: recipient.id}) ));
+      setProducts(() => productList.map(product => ({name: product.title, id: product.id }) ));
+      
+      var selects = document.querySelectorAll('select');
+      M.FormSelect.init(selects);
     }
-    effect();
+    selectEffect();
   }, []);
+
+  useEffect(() => {
+    var elems = document.querySelectorAll('.modal');
+    console.log(elems);
+    var [instance] = M.Modal.init(elems, {
+      onCloseEnd: () => {
+        // display done and redirect to storage center?
+        M.toast({html: 'Shipment created'});
+        history.push("/storage-center");
+      }
+    });
+    if(instance) instance.open();
+  }, [code, history]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    formData.timestamp = (new Date()).toISOString();
     console.log(formData);
+    
     // TODO: formData to be sent to backend api
+    axios.post("/api/transfer", formData)
+      .then(res => {
+        console.log(res.data);
+        setCode(res.data);
+      });
   }
 
   const handleChange = (e) => {
@@ -37,7 +63,7 @@ export default function SendShipment() {
 
   return (
     <div className="row">
-      <h2>Send a shipment</h2>
+      <h2 className="center-align">Send a shipment</h2>
       <form className="offset-s2 col s8">
       <div className="row">
           <div className="input-field">
@@ -69,6 +95,16 @@ export default function SendShipment() {
         </div>
         
       </form>
+      {code !== "" ? <div id="modal1" className="modal">
+        <div className="modal-content">
+          <h4>Shipment Created</h4>
+          <h5>To start the shipment, give this code to the person in charge of moving the products to the destination</h5>
+          <h2 className="center-align">{code}</h2>
+        </div>
+        <div className="modal-footer">
+          <a href="#!" className="modal-close waves-effect waves-green btn-flat">Done</a>
+        </div>
+      </div> : null}
     </div>
   )
 }
--- client/src/components/storageCenter/StorageCenter.jsx
+++ client/src/components/storageCenter/StorageCenter.jsx
@@ -8,19 +8,38 @@ import Inventory from "./Inventory";
 
 export default function StorageCenter() {
   return (
-    <div>
-      <h1>Storage Center</h1>
-      <Link to="/storage-center/send-shipment">Send Shipment</Link>
-      <Link to="/storage-center/receive-shipment">Receive Shipment</Link>
-      <Link to="/storage-center/incoming-shipments">Incoming Shipments</Link>
-      <Link to="/storage-center/outgoing-shipment">Outgoing Shipments</Link>
-      <Link to="/storage-center/inventory">Inventory</Link>
+    <div className="container">
+      <Route exact path="/storage-center">
+        <h1 className="center-align">Storage Center</h1>
+        <div className="row center-align">
+          <div className="card col s4 offset-s2">
+            <Link to="/storage-center/send-shipment"><h5>Send Shipment</h5></Link>
+          </div>
+          <div className="card col s4">
+            <Link to="/receive-shipment"><h5>Receive Shipment</h5></Link>
+          </div>
+        </div>
+        <div className="row center-align">
+          <div className="card col s4 offset-s2">
+            <Link to="/incoming-shipments"><h5>Incoming Shipments</h5></Link>
+          </div>
+          <div className="card col s4">
+            <Link to="/outgoing-shipment"><h5>Outgoing Shipments</h5></Link>
+          </div> 
+        </div>
+        <div className="row center-align">
+          <div className="card col s8 offset-s2">
+            <Link to="/inventory"><h5>Inventory</h5></Link>
+          </div>
+        </div>
+      </Route>
+      
 
       <Route path="/storage-center/send-shipment" component={SendShipment}/>
-      <Route path="/storage-center/receive-shipment" component={ReceiveShipment}/>
-      <Route path="/storage-center/incoming-shipments" component={IncomingShipments}/>
-      <Route path="/storage-center/outgoing-shipment" component={OutgoingShipments}/>
-      <Route path="/storage-center/inventory" component={Inventory}/>
+      <Route path="/receive-shipment" component={ReceiveShipment}/>
+      <Route path="/incoming-shipments" component={IncomingShipments}/>
+      <Route path="/outgoing-shipment" component={OutgoingShipments}/>
+      <Route path="/inventory" component={Inventory}/>
 
     </div>
   );
