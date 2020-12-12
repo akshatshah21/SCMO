9cbcd9198222d9a411277d6bfe702bc7e2c06caa
Akshat Shah <akshatshah_21@yahoo.in>
2020-12-12 17:44:32 +0530

    Start and end time display in Shipment.jsx
    Because of Neo4j returning it that way apparently

--- client/src/components/storageCenter/Shipment.jsx
+++ client/src/components/storageCenter/Shipment.jsx
@@ -35,26 +35,32 @@ export default function Shipment({
   useEffect(() => {
     let getTransfer = async () => {
       try {
+        let { data: transfer } = await axios.get(
+          "/api/transfer/" + shipmentId + "/details"
+        );
+        console.log(transfer);
 
-        let {data: transfer} = await axios.get("/api/transfer/" + shipmentId + "/details");
-        // console.log(transfer);
+        transfer.transferStartTime = `${transfer.transferStartTime.day}/${transfer.transferStartTime.month}/${transfer.transferStartTime.year} ${transfer.transferStartTime.hour}:${transfer.transferStartTime.minute}`;
+        if (transfer.transferEndTime) {
+          transfer.transferEndTime = `${transfer.transferEndTime.day}/${transfer.transferEndTime.month}/${transfer.transferEndTime.year} ${transfer.transferEndTime.hour}:${transfer.transferEndTime.minute}`;
+        } else {
+          transfer.transferEndTime = "NA";
+        }
 
         setShipmentDetails({
           sourceName: transfer.source.stageName,
           destinationName: transfer.destination.stageName,
           sourceEmail: transfer.source.stageEmail,
           destinationEmail: transfer.destination.stageEmail,
-          startTime: transfer.transferStartTime,
-          endTime: transfer.transferEndTime,
           sourceLocation: transfer.source.location,
           destinationLocation: transfer.destination.location,
           sourceAddress: transfer.source.stageAdd,
           destinationAddress: transfer.destination.stageAdd,
           status: transfer.transferStatus,
+          startTime: transfer.transferStartTime,
+          endTime: transfer.transferEndTime,
         });
-
         setProducts(transfer.products);
-
       } catch (error) {
         console.log(error);
         M.toast({ html: "Error" });
@@ -63,7 +69,7 @@ export default function Shipment({
     getTransfer();
   }, [shipmentId]);
 
-  const addRouteToMap = geojson => {
+  const addRouteToMap = (geojson) => {
     if (map.getSource("route")) {
       map.getSource("route").setData(geojson);
     } else {
@@ -92,7 +98,7 @@ export default function Shipment({
         },
       });
     }
-  }
+  };
 
   useEffect(() => {
     mapboxgl.accessToken = MAPBOX_API_TOKEN;
@@ -132,7 +138,7 @@ export default function Shipment({
             coordinates: route,
           },
         };
-        
+
         // Problematic part
         // Route is not loaded the first time, page needs to be refreshed!
         map.on("style.load", () => {
@@ -143,14 +149,14 @@ export default function Shipment({
         map.on("styledata", () => {
           // console.log("Style");
           addRouteToMap(geojson);
-        })
+        });
       };
       renderRoute();
     }
   }, [shipmentDetails]);
 
   useEffect(() => {
-    if(shipmentDetails.status === "ongoing") {
+    if (shipmentDetails.status === "ongoing") {
       const socket = io(ENDPOINT);
       socket.on("connect", () => {
         // console.log("SocketIO client connected to server");
@@ -167,13 +173,13 @@ export default function Shipment({
   }, [shipmentId, shipmentDetails]);
 
   useEffect(() => {
-    if(shipmentDetails.status === "ongoing") {
-      setShipmentMarker(prevMarker => {
-        if(prevMarker) prevMarker.remove();
+    if (shipmentDetails.status === "ongoing") {
+      setShipmentMarker((prevMarker) => {
+        if (prevMarker) prevMarker.remove();
         return new mapboxgl.Marker({ color: "#ff1744" })
-        .setLngLat([shipmentLocation.longitude, shipmentLocation.latitude])
-        .setPopup(shipmentPopup)
-        .addTo(map)
+          .setLngLat([shipmentLocation.longitude, shipmentLocation.latitude])
+          .setPopup(shipmentPopup)
+          .addTo(map);
       });
     }
   }, [shipmentLocation, shipmentDetails]);
@@ -205,7 +211,9 @@ export default function Shipment({
           <h4 className="red-text center">Ongoing</h4>
         ) : null}
         <p>Start: {shipmentDetails.startTime}</p>
-        {shipmentDetails.status === "completed" && <p>End: {shipmentDetails.endTime}</p>}
+        {shipmentDetails.status === "completed" && (
+          <p>End: {shipmentDetails.endTime}</p>
+        )}
         <h5>Products</h5>
         <table>
           <tbody>
