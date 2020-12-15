109bcd64eda506f6c5a6815deba2844a012a8f76
Akshat Shah <akshatshah_21@yahoo.in>
2020-12-15 21:34:50 +0530

    Add keys back to gitignore

--- api/neo4j-db/index.js
+++
@@ -1,5 +0,0 @@
-let product = require("./product");
-let connection = require("./connection");
-
-product.getMostTransferredProducts("0e933ff0-3c55-11eb-bfca-4156269f0da7");
-connection.getMostDemandingConnections("b3c59ee0-3be3-11eb-8d16-8f89a8bf45e4");
